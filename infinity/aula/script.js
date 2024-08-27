ac3d130b266845023602563641b33a61a356c099
Liandrolima <156594619+Liandrolima@users.noreply.github.com>
2024-08-27 14:09:03 -0300

    infinity1

---
+++ infinity/aula/script.js
@@ -0,0 +1,7 @@
+/*alert("Deu certo")
+console.log("Deu certo")
+document.write("Deu certo no documento")*/
+/*FORMAS DE DECLARAR*/
+var nome = "liandro"//NUNCA USE(ESCOPO GLOBAL)
+let nome2 = "liandro"//ESCOPO LOCAL(SE PODE MUDA O VALOR)
+const nome3 = "liandro"//ESCOPO LOCAL(NÃO SE PODE MUDA O VALOR)
