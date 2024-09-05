831e3a88578807df3f99e347204655ecbfda8ad1
Liandrolima <156594619+Liandrolima@users.noreply.github.com>
2024-09-05 19:03:58 -0300

    operações aritméticas

---
+++ infinity/aula1/exer5.5/script.js
@@ -0,0 +1,14 @@
+//Atividade 05
+//Crie um código JavaScript que utilize operadores aritméticos para somar, subtrair,
+//multiplicar e/ou dividir dois números fornecidos pelo usuário através de prompt.
+//Em seguida, exiba os resultados de cada operação usando a função alert.
+const n1 = Number(prompt("DIgite um número "))
+const n2 = Number(prompt("DIgite um número "))
+const soma = n1 + n2
+const subtrair = n1 - n2
+const multiplicar = n1 * n2
+const dividir = n1 / n2
+alert(`A soma : ${soma}`)
+alert(`A subtração é: ${subtrair}`)
+alert(`A multiplicação é: ${multiplicar}`)
+alert(`A divisão é: ${dividir}`)
